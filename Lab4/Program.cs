0bfb793169d328cf24f477b654a4f179b1f8b10f
teresav3 <tvargin@gmail.com>
2025-04-15 02:15:44 +0000

    Committed

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
+    //Problem 1
+        for (int i = 1; i <=10; i++) 
+        {
+        Console.WriteLine(i);
+        }
+    
+    //Problem 2
+        for (int i = 1; i <=20; i++)
+        
+        if (i % 2 == 0)
+        {
+        Console.WriteLine(i);
+        }
+    
+    //Problem 3
+        int number = 5;
+
+        while (number >= 1)
+        {
+        Console.WriteLine(number);
+        number--;
+        }
+
+    //Problem 4
+        int number1 = 10;
+
+        while (number1 <= 1000)
+        {
+        Console.WriteLine(number1);
+        number1 += 10;
+        }
+
+    //Problem 5
+        for (int row = 1; row <= 10; row++)
+        {
+        for (int col = 1; col <= row; col++) 
+            {
+            Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    } 
+
 }
